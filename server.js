db8e653d769c9409fd5ce6f5f9bd94ccaa6d339d
YanchikCherryz <yanacherenenko@mail.ru>
2024-03-14 15:23:45 +0500

    init app

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const app = express()
+const port = 25565
+
+app.use(express.static('frontend/dist'))
+
+app.listen(port, () => console.log('Server has been started on port 25565'))
